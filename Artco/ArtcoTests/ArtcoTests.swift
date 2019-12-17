b64159ca6ecb66185ece3211d2f590abd504cd80
Mitchell Budge <mitchellbudge@gmail.com>
2019-12-17 12:31:50 -0700

    Create initial Xcode project

---
+++ Artco/ArtcoTests/ArtcoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ArtcoTests.swift
+//  ArtcoTests
+//
+//  Created by Mitchell Budge on 12/17/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import XCTest
+@testable import Artco
+
+class ArtcoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
