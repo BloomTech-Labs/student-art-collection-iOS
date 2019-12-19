08cceae42b950ba800f23335e62b7fa517415e62
Mitchell Budge <mitchellbudge@gmail.com>
2019-12-19 13:59:02 -0700

    Configure convenience initializer for Listing object

--- Artco/Artco/Model Controller/ListingController.swift
+++ Artco/Artco/Model Controller/ListingController.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+class ListingController {
+    
+    func createListing() {
+        
+    }
+    
+    
+    
+}
--- Artco/Artco/Model/Model Representations/Listing+Convenience.swift
+++ Artco/Artco/Model/Model Representations/Listing+Convenience.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import CoreData
+
+extension Listing {
+    
+    @discardableResult convenience init (title: String,
+                                         sold: Bool = false,
+                                         schoolId: Float,
+                                         price: Float = 25.00,
+                                         category: ListingCategory,
+                                         artistName: String = "Anonymous",
+                                         artDescription: String = "No description provided",
+                                         images: [String],
+                                         context: NSManagedObjectContext = NSManagedObjectContext.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.sold = sold
+        self.schoolId = schoolId
+        self.price = price
+        self.category = category.rawValue
+        self.artistName = artistName
+        self.images = images
+        self.artDescription = artDescription
+    }
+    
+}
