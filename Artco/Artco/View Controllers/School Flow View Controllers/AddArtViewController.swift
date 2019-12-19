35a3c89d0200347360234d029168b5e2030bef55
Mitchell Budge <mitchellbudge@gmail.com>
2019-12-19 16:05:59 -0700

    Integrate Photo Library access for image selection

---
+++ Artco/Artco/View Controllers/School Flow View Controllers/AddArtViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddArtViewController.swift
+//  Artco
+//
+//  Created by Mitchell Budge on 12/19/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class AddArtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
