28c228329e6df11f9733149f2c0bdd0e536b01db
Mitchell Budge <mitchellbudge@gmail.com>
2019-12-19 13:28:19 -0700

    Create Listing Core Data model object

---
+++ Artco/Artco/Helpers/Category.swift
@@ -0,0 +1,17 @@
+//
+//  Category.swift
+//  Artco
+//
+//  Created by Mitchell Budge on 12/19/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+enum ListingCategory : Float {
+    case Photography = 1
+    case Drawing = 2
+    case Painting = 3
+    case Sculpture = 4
+    case Other = 5
+}
