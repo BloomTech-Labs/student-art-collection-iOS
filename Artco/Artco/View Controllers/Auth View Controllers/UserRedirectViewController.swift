94a15ef7a1b21fb3372313a860dafda077bc9d34
Mitchell Budge <mitchellbudge@gmail.com>
2020-01-08 16:59:08 -0700

    Eliminate unnecessary placeholder methods

--- Artco/Artco/View Controllers/Auth View Controllers/UserRedirectViewController.swift
+++ Artco/Artco/View Controllers/Auth View Controllers/UserRedirectViewController.swift
@@ -10,12 +10,6 @@ import UIKit
 
 class UserRedirectViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
     @IBAction func buyerRedirectButtonTapped(_ sender: UIButton) {
         DispatchQueue.main.async {
             let storyboard = UIStoryboard(name: "BuyerFlow", bundle: nil)
@@ -24,15 +18,5 @@ class UserRedirectViewController: UIViewController {
             self.present(buyerTabBarController, animated: true, completion: nil)
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
