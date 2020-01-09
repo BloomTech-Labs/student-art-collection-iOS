2600d78a65a279b1694940a93978fe777639a703
Mitchell Budge <mitchellbudge@gmail.com>
2020-01-09 15:18:30 -0700

    Create controller class to manage buyer cart selections

---
+++ Artco/Artco/Model Controller/BuyerController.swift
@@ -0,0 +1,22 @@
+//
+//  BuyerController.swift
+//  Artco
+//
+//  Created by Mitchell Budge on 1/9/20.
+//  Copyright © 2020 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+import Apollo
+
+class BuyerController {
+    
+    static let shared = BuyerController()
+    
+    var cart: [ArtQuery.Data.Art] = []
+    
+    func addToCart(_ listing: ArtQuery.Data.Art) {
+        cart.append(listing)
+    }
+    
+}
