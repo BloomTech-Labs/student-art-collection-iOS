653e4156d7b7905d0aec63220c8418389345e4f5
Mitchell Budge <mitchellbudge@gmail.com>
2020-01-13 12:19:03 -0700

    Create custom subclass for adding school information

---
+++ Artco/Artco/View Controllers/Auth View Controllers/SchoolSignUp2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SchoolSignUp2ViewController.swift
+//  Artco
+//
+//  Created by Mitchell Budge on 1/13/20.
+//  Copyright © 2020 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class SchoolSignUp2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
