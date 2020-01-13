48a664540f7f443cd50d6538be6504750f80c040
Mitchell Budge <mitchellbudge@gmail.com>
2020-01-13 13:20:33 -0700

    Finalize add school method to POST to server

--- Artco/Artco/View Controllers/Auth View Controllers/SchoolSignUp2ViewController.swift
+++ Artco/Artco/View Controllers/Auth View Controllers/SchoolSignUp2ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 import Apollo
 
 class SchoolSignUp2ViewController: UIViewController {
@@ -18,13 +19,28 @@ class SchoolSignUp2ViewController: UIViewController {
     
     @IBAction func doneButtonTapped(_ sender: UIButton) {
         
+        guard let schoolId = Auth.auth().currentUser?.uid,
+            !schoolId.isEmpty,
+            let email = Auth.auth().currentUser?.email,
+            !email.isEmpty,
+            let schoolName = schoolNameTextField.text,
+            !schoolName.isEmpty,
+            let address = addressTextField.text,
+            !address.isEmpty,
+            let city = cityTextField.text,
+            !city.isEmpty,
+            let zipcode = zipcodeTextField.text,
+            !zipcode.isEmpty else { return }
+        
+        SchoolController.shared.createSchool(schoolId: schoolId, schoolName: schoolName, email: email, address: address, city: city, zipcode: zipcode)
+        
+        Network.shared.apollo.perform(mutation: AddSchoolMutation(school_id: schoolId, school_name: schoolName, email: email, address: address, city: city, zipcode: zipcode))
         
         DispatchQueue.main.async {
             let storyboard = UIStoryboard(name: "SchoolFlow", bundle: nil)
             let schoolTabBarController = storyboard.instantiateViewController(withIdentifier: "SchoolTabBarController")
             schoolTabBarController.modalPresentationStyle = .fullScreen
             self.present(schoolTabBarController, animated: true, completion: nil)
-            
         }
     }
     
