2bc5045bcb7b5fdd5023498e301a543e7b53848e
Mitchell Budge <mitchellbudge@gmail.com>
2020-01-13 12:51:48 -0700

    Structure convenience file for school representation

--- Artco/Artco/View Controllers/Auth View Controllers/SchoolSignUp2ViewController.swift
+++ Artco/Artco/View Controllers/Auth View Controllers/SchoolSignUp2ViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import Apollo
 
 class SchoolSignUp2ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
+    
+    
     
 
     /*
