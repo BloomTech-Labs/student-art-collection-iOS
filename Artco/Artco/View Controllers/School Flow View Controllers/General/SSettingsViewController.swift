4fbc9f65cec6900f1237d61c7d32738a359fc6d5
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-01-14 10:21:19 -0600

    Update title position - school

--- Artco/Artco/View Controllers/School Flow View Controllers/General/SSettingsViewController.swift
+++ Artco/Artco/View Controllers/School Flow View Controllers/General/SSettingsViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class SSettingsViewController: UIViewController {
 
+    @IBOutlet weak var schoolNameTextfield: UITextField!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    */
 
 }
