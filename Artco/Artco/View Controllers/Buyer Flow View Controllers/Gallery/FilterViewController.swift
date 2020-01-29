bcecc85a11fed43f3296357538d18d98a751a563
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-01-28 22:45:59 -0600

    Add filter VC

---
+++ Artco/Artco/View Controllers/Buyer Flow View Controllers/Gallery/FilterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FilterViewController.swift
+//  Artco
+//
+//  Created by Alex Rhodes on 1/28/20.
+//  Copyright © 2020 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    @IBOutlet weak var categoryTextField: UITextField!
+    @IBOutlet weak var zioCodeTextField: UITextField!
+    @IBOutlet weak var schoolNameCategory: UITextField!
+    @IBOutlet weak var artistNameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
